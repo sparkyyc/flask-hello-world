fba52d8f71c8c91ae2d59f9547ec4ff57a571962
sparkyyc <sparkyyc@gmail.com>
2024-07-29 20:06:42 -0600

    requirments fixed, test db conn

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Christa Sparks in 3308'
 
-# @app.route('/db_test')
-# def db_test():
-#    conn = psycopg2.connect("postgresql://sparkyyc_db_user:v0J6Bm3YGWmxYFcWuSmckXaRXohUyh3P@dpg-cqk3hujqf0us73c29050-a/sparkyyc_db")
-#    conn.close()
-#    return 'Database connection successful'
+@app.route('/db_test')
+def db_test():
+   conn = psycopg2.connect("postgresql://sparkyyc_db_user:v0J6Bm3YGWmxYFcWuSmckXaRXohUyh3P@dpg-cqk3hujqf0us73c29050-a/sparkyyc_db")
+   conn.close()
+   return 'Database connection successful'
