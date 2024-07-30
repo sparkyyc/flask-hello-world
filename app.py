194d8b2ae85097865dd0d69f605b8b651bff4465
sparkyyc <sparkyyc@gmail.com>
2024-07-29 20:36:48 -0600

    added db_create

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ def db_test():
    conn = psycopg2.connect("postgresql://sparkyyc_db_user:v0J6Bm3YGWmxYFcWuSmckXaRXohUyh3P@dpg-cqk3hujqf0us73c29050-a/sparkyyc_db")
    conn.close()
    return 'Database connection successful'
+
+@app.route('db_create')
+def db_create():
+   conn = psycopg2.connect("postgresql://sparkyyc_db_user:v0J6Bm3YGWmxYFcWuSmckXaRXohUyh3P@dpg-cqk3hujqf0us73c29050-a/sparkyyc_db")
+   cur = conn.cursor()
+   cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+   conn.commit()
+   conn.close()
+   return 'Basketball Table created successfully'
